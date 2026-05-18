84ddf9a8daff274d7d02f02ef233f8f0470ce997
ExZel <deekaewt@gmail.com>
2026-05-18 22:06:55 +0700

    Create multiplication table2.c

---
+++ multiplication table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    bool a = true;
+    int rows;
+    int columns;
+    while(a){
+        printf("How many rows for your table: ");
+        scanf("%d",&rows);
+        printf("\n");
+        printf("How many columns for your table: ");
+        scanf("%d",&columns);
+        printf("\n");
+    }
+}
